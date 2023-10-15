1520088c9ef35fbe8eb62f8751da8e273792e975
umitkaraduman <umtkrdmn@gmail.com>
2023-10-15 11:52:44 -0700

    Last version

---
+++ ServiceWorker.js
@@ -0,0 +1,34 @@
+const cacheName = "DefaultCompany-Oceanland1MiniGame-1.0";
+const contentToCache = [
+    "Build/webgl.loader.js",
+    "Build/webgl.framework.js",
+    "Build/webgl.data",
+    "Build/webgl.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    if (!(evt.request.url.indexOf('http') === 0)) return; 
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
