55288a0df18e9c1d1aea16bf1335e0c0ec31f05a
umitkaraduman <umtkrdmn@gmail.com>
2023-10-17 11:28:33 -0700

    Test

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -19,6 +19,7 @@ self.addEventListener('install', function (e) {
 });
 
 self.addEventListener('fetch', function (e) {
+    if (!(evt.request.url.indexOf('http') === 0)) return; 
     e.respondWith((async function () {
       let response = await caches.match(e.request);
       console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
